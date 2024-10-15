6bc3c8d13b00e2a03d789f9c8091ffbbd6d0bacf
SvintsovAnton <Svintsov.anton94@gmail.com>
2024-10-15 15:09:29 +0200

    addChange

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -2,20 +2,60 @@ import { Header, User } from "components/index"
 import { ProfileWrapper, InfoContainer, Title } from "./styles"
 import { UserContainer } from "pages/Settings/styles"
 import { UserTestImg } from "assets"
+import { useState, useEffect } from "react"
+import UserInterface from "pages/Profile/ProfileIntrface";
+import ApiError from "pages/ApiError/ApiError";
 export default function Profile() {
+
+const [user,setUser] = useState<UserInterface |null >(null);
+const [loading,setLoading] = useState<boolean>(true);
+const[error,setError] = useState<Error | null >(null);
+
+
+useEffect (()=>{
+
+  const fetchData = async () =>{
+try{
+  const response = await fetch(`/api/users/current`);
+  if (!response.ok){
+    throw new Error('Network response was not ok');
+  } 
+  const user = await response.json();
+  setUser(user);
+} catch (error:unknown){
+  if (error instanceof Error){
+  setError(error);
+  }
+}
+finally {
+  setLoading(false);
+}
+  };
+ fetchData();
+},[]);
+
+
   return (
     <ProfileWrapper>
       <Header></Header>
+      {loading ? (
+      <p>Loading...</p>
+    ) : error ? (
+      <ApiError />
+    ) : user ? (
+      <>
       <UserContainer>
-        <User username={"Guest"} imagePath={UserTestImg} />
+        <User username={user.firstName} imagePath={UserTestImg} />
       </UserContainer>
       <InfoContainer>
-        <Title>👤 Ben Ben</Title>
-        <Title>📧 benAdmin@example.com</Title>
-        <Title>📞 +123456789</Title>
-        <Title>📅 1995-01-01</Title>
-        <Title>⚧️ Male</Title>
+        <Title>👤{user.firstName} {user.lastName} </Title>
+        <Title>📧 {user.email}</Title>
+        <Title>📞 {user.phone}</Title>
+        <Title>📅 {user.dateOfBirth}</Title>
+        <Title>⚧️ {user.gender}</Title>
       </InfoContainer>
+      </>):null}
+     
     </ProfileWrapper>
   )
 }
